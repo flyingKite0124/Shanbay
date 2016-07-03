db18943d95b1f7dcfd37fc7355cecb47a93cb7a5
ShiPeng <spone.2014@gmail.com>
2016-07-04 00:20:42 +0800

    second version

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,20 +1,287 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from database.models import *
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect,HttpResponseNotAllowed, JsonResponse
 import json
+import const
+
+const.restaurant = {"UNCERTIFIED": 0, "OPENING": 1, "CLOSED": 2, "FROZEN": 3}
+const.order = {
+    "UNCERTAIN": 0,
+    "CERTAIN": 1,
+    "PAYED": 2,
+    "ACCEPTED": 3,
+    "SENT": 4,
+    "FINISHED": 5}
 
 # Create your views here.
 def sign(request):
-    # request.session["issigned"]=True
-    return render(request,"restaurant/sign.html")
+    if not request.is_ajax() and request.method == "GET":
+        return render(request,"restaurant/sign.html")
+    else:
+        return HttpResponseNotAllowed(['GET'], 'illegal request')
+
+def signIn(request):
+    if request.is_ajax() and request.method == "POST":
+        data = json.loads(request.body)
+        if "phone" not in data or "passwd" not in data:
+            return JsonResponse({"result":"fail"})
+        else:
+            phone = data["phone"]
+            passwd = data["passwd"]
+            try:
+                restaurant = Restaurant.objects.get(phone=phone)
+            except Exception:
+                return JsonResponse({"result":"fail"})
+            if restaurant == None:
+                return JsonResponse({"result":"fail"})
+            elif restaurant.passwd == passwd:
+                request.session["mID"] = restaurant.id
+                request.session["isSignIn"] = True
+                return JsonResponse({"result":"success"})
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+
+def signUp(request):
+    if request.is_ajax() and request.method == "POST":
+        data = json.loads(request.body)
+        if "phone" not in data \
+        or "passwd" not in data \
+        or "restaurant_name" not in data \
+        or "introduction" not in data \
+        or "address" not in data \
+        or "classification" not in data:
+            return JsonResponse({"result":"fail"})
+        phone = data["phone"]
+        passwd = data["passwd"]
+        restaurant_name = data["restaurant_name"]
+        introduction = data["introduction"]
+        address = data["address"]
+        classification = data["classification"]
+        if len(Restaurant.objects.filter(phone=phone) > 0):
+            return JsonResponse({"result": "fail"})
+        else:
+            try:
+                newRestaurant = Restaurant()
+                newRestaurant.phone = phone
+                newRestaurant.passwd = passwd
+                newRestaurant.restaurant_name = restaurant_name
+                newRestaurant.introduction = introduction
+                newRestaurant.address = address
+                newRestaurant.classification = classification
+                newRestaurant.save()
+                return JsonResponse({"result":"fail"})
+            except Exception:
+                return JsonResponse({"result":"success"})
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+
+def checkPhone(request):
+    if request.is_ajax() and request.method == "POST":
+        data = json.loads(request.body)
+        if "phone" not in data:
+            return JsonResponse({"result":"fail"})
+        phone = data["phone"]
+        duplicate = Restaurant.objects.filter(phone = phone)
+        if (len(duplicate) > 0):
+            return JsonResponse({"result":"fail"})
+        else:
+            return JsonResponse({"result":"success"})
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+    
+def checkName(request):
+    if request.is_ajax() and request.method == "POST":
+        data = json.loads(request.body)
+        if "restaurant_name" not in data:
+            return JsonResponse({"result":"fail"})
+        restaurant_name = data["restaurant_name"]
+        duplicate = Restaurant.objects.filter(restaurant_name = restaurant_name)
+        if (len(duplicate) > 0):
+            return JsonResponse({"result":"fail"})
+        else:
+            return JsonResponse({"result":"success"})
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
 
 def manage(request):
-    # request.session["issigned"]=False
-    restaurant = Restaurant.objects.get(name="SiLing")
-    content_dict = dict()
-    content_dict["restaurant"] = restaurant
-    request.session["mRestaurant"] = restaurant.name
-    return render(request,"restaurant/manage.html", content_dict)
+    if not request.session.get("isSignIn", False):
+        return HttpResponseRedirect("sign")
+    if not request.is_ajax() and request.method == "GET":
+        restaurant = None
+        try:
+            restaurant = Restaurant.objects.get(id=request.session.get("mID"))
+        except Exception:
+            HttpResponseRedirect("sign")
+        if restaurant == None:
+            HttpResponseRedirect("sign")
+        return render(request,"restaurant/manage.html", {"restaurant":restaurant})
+    else:
+        return HttpResponseNotAllowed(['GET'], 'illegal request')
+
+def updateInformation(request):
+    if not request.session.get("isSignIn", False):
+        return HttpResponseRedirect("sign")
+    if request.is_ajax() and request.method == "POST":
+        restaurant = None
+        try:
+            restaurant = Restaurant.objects.get(id=request.session.get("mID"))
+        except Exception:
+            HttpResponseRedirect("sign")
+        if restaurant == None:
+            HttpResponseRedirect("sign")
+        data = json.loads(request)
+        if "restaurant_name" not in data \
+        or "phone" not in data \
+        or "introduction" not in data \
+        or "address" not in data \
+        or "classification" not in data:
+            return JsonResponse({"result":"fail"})
+        restaurant_name = data["restaurant_name"]
+        if restaurant_name != None and restaurant_name != "":
+            restaurant.restaurant_name = restaurant_name
+        phone = data["phone"]
+        if phone != None and phone != "":
+            restaurant.phone = phone
+        introduction = data["introduction"]
+        if introduction != None and introduction != "":
+            restaurant.introduction = introduction
+        address = data["address"]
+        if address != None and address != "":
+            restaurant.address = address
+        classification = data["classification"]
+        if classification != None and classification != "":
+            restaurant.classification = classification
+        restaurant.save()
+        JsonResponse({"result":"success"})
+
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+
+def changePasswd(request):
+    if not request.session.get("isSignIn", False):
+        return HttpResponseRedirect("sign")
+    if request.is_ajax() and request.method == "POST":
+        restaurant = None
+        try:
+            restaurant = Restaurant.objects.get(id=request.session.get("mID"))
+        except Exception:
+            HttpResponseRedirect("sign")
+        if restaurant == None:
+            HttpResponseRedirect("sign")
+        data = json.loads(request.body)
+        if "old_password" not in data or "new_password" not in data:
+            return JsonResponse({"result":"fail"})
+        else:
+            old_password = data["old_password"]
+            new_password = data["new_password"]
+            if restaurant.passwd != old_password:
+                return JsonResponse({"result":"fail"})
+            else:
+                restaurant.passwd = new_password
+                restaurant.save()
+                return JsonResponse({"result":"success"})
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+
+def changeStatus(request):
+    if not request.session.get("isSignIn", False):
+        return HttpResponseRedirect("sign")
+    if request.is_ajax() and request.method == "POST":
+        restaurant = None
+        try:
+            restaurant = Restaurant.objects.get(id=request.session.get("mID"))
+        except Exception:
+            HttpResponseRedirect("sign")
+        if restaurant == None:
+            HttpResponseRedirect("sign")
+        data = json.loads(request.body)    
+        if "type" not in data:
+            return JsonResponse({"result":"fail"})
+        else:
+            global const
+            mType = data["type"]
+            if mType == "open":
+                restaurant.status = const.restaurant["OPENING"]
+            if mType == "close":
+                restaurant.status = const.restaurant["CLOSED"]
+            restaurant.save()
+            return JsonResponse({"result":"success"})
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+
+def manageDish(request):
+    if not request.session.get("isSignIn", False):
+        return HttpResponseRedirect("sign")
+    if request.is_ajax() and request.method == "POST":
+        restaurant = None
+        try:
+            restaurant = Restaurant.objects.get(id=request.session.get("mID"))
+        except Exception:
+            HttpResponseRedirect("sign")
+        if restaurant == None:
+            HttpResponseRedirect("sign")
+        data = json.loads(request.body)
+        if "type" in data:
+            mType = data["type"]
+            if mType == "new":
+                if "name" not in data \
+                or "price" not in data \
+                or "introduction" not in data:
+                    return JsonResponse({"result":"fail"})
+                name = data["name"]
+                price = data["price"]
+                introduction = data["introduction"]
+                try:
+                    dish = Dish(name = name, price = float(price), introduction = introduction, restaurant = restaurant)
+                    dish.save()
+                    return JsonResponse({"result","success"})
+                except Exception:
+                    return JsonResponse({"result","fail"})
+            elif mType == "update":
+                if "dish_id" not in data:
+                    return JsonResponse({"result":"fail"})
+                dish_id = data["dish_id"]
+                try:
+                    dish = Dish.objects.get(id=dish_id)
+                except Exception:
+                    return JsonResponse({"result","fail"})
+                if "price" not in data \
+                or "introduction" not in data:
+                    return JsonResponse({"result","fail"})
+                try:
+                    price = data["price"]
+                    if price != None and price != "":
+                        dish.price = float(price)
+                    introduction = data["introduction"]
+                    if introduction != None and introduction != ""
+                        dish.introduction = introduction
+                    dish.save()
+                    return JsonResponse({"result":"success"})
+                except Exception:
+                    return JsonResponse({"result":"fail"})
+            elif mType == "delete":
+                if "dish_id" not in data:
+                   return JsonResponse({"result":"fail"})
+                dish_id = data["dish_id"]
+                try:
+                    dish = Dish.objects.get(id=dish_id)
+                    dish.delete()
+                    return JsonResponse("result":"success")
+                except Exception:
+                    return JsonResponse("result":"fail")
+            else:
+                return JsonResponse("result":"fail")
+        else:
+            return JsonResponse("result":"fail")
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+
+def pollOrder(request):
+    return HttpResponse("pollOrder")
+
+
 
 def redirect(request):
     if request.session.get("issigned",False):
@@ -22,132 +289,53 @@ def redirect(request):
     else:
         return HttpResponseRedirect("sign")
 
-def signIn(request):
-    content_dict = dict()
-    content_dict["result"] = "success"
-    result = json.dumps(content_dict)
-    return HttpResponse(result)
-    '''
-    if not request.is_ajax() and request.method == "GET":
-        return render(request,"restaurant/signIn.html",content_dict)
-    elif request.is_ajax() and request.method == "POST":
-        return render(request,"restaurant/signIn.html",content_dict)
-    '''
-
-def signUp(request):
-    content_dict = dict()
-    content_dict["result"] = "success"
-    result = json.dumps(content_dict)
-    return HttpResponse(result)
-
-def checkPhone(request):
-    content_dict = dict()
-    content_dict["result"] = "success"
-    result = json.dumps(content_dict)
-    return HttpResponse(result)
-
-def checkName(request):
-    content_dict = dict()
-    content_dict["result"] = "success"
-    result = json.dumps(content_dict)
-    return HttpResponse(result)
-
-
-def updateInformation(request):
-    return HttpResponseRedirectnse("updateInformation")
-
-def changePasswd(request):
-    return HttpResponse("changePasswd")
-
-def changeStatus(request):
-    content_dict = dict()
-    content_dict["result"] = "success"
-    result = json.dumps(content_dict)
-    return HttpResponse(result)
-
-def manageDish(request):
-    content_dict = dict()
-    if request.is_ajax() and request.method == "POST":
-        if "type" in request.POST:
-            mType = request.POST["type"]
-            if mType == "new":
-                if "name" not in request.POST \
-                or "price" not in request.POST \
-                or "introduction" not in request.POST:
-                    content_dict["result"] = "fail"
-                    result = json.dumps(content_dict)
-                    return HttpResponse(result)
-                print "finish add check"
-                name = request.POST["name"]
-                price = request.POST["price"]
-                introduction = request.POST["introduction"]
-                print name, price, introduction
-                restaurantName = request.session.get("mRestaurant")
-                restaurant = Restaurant.objects.get(name=restaurantName)
-                print "Restaurant name : ", restaurant.name
-                dish = Dish(name = name, price = float(price), introduction = introduction, restaurant = restaurant)
-                print "create succ"
-                dish.save()
-                print "save succ"
-                print "name : " ,dish.name 
-                print "price : ", dish.price
-                print "introduction : ", introduction
-                content_dict["result"] = "success"
-                result = json.dumps(content_dict)
-                return HttpResponse(result)
-            elif mType == "update":
-                print "updating"
-                if "dish_id" not in request.POST:
-                    print "no dish id"
-                    content_dict["result"] = "fail"
-                    result = json.dumps(content_dict)
-                    return HttpResponse(result)
-                dish_id = request.POST["dish_id"]
-                dish = Dish.objects.get(id=dish_id)
-                if "price" in request.POST:
-                    print "before : ", dish.price
-                    dish.price = float(request.POST["price"])
-                    print "after : ",dish.price
-                if "introduction" in request.POST:
-                    print dish.introduction
-                    dish.introduction = request.POST["introduction"]
-                dish.save()
-                print "finish update"
-                content_dict["result"] = "success"
-                result = json.dumps(content_dict)
-                return HttpResponse(result)
-            elif mType == "delete":
-                print "deleting"
-                if "dish_id" not in request.POST:
-                    print "no dish id"
-                    content_dict["result"] = "fail"
-                    result = json.dumps(content_dict)
-                    return HttpResponse(result)
-                dish_id = request.POST["dish_id"]
-                dish = Dish.objects.get(id=dish_id)
-                dish.delete()
-                content_dict["result"] = "success"
-                result = json.dumps(content_dict)
-                return HttpResponse(result)
-            else:
-                content_dict["result"] = "fail"
-                result = json.dumps(content_dict)
-                return HttpResponse(result)
-        else:
-            content_dict["result"] = "fail"
-            result = json.dumps(content_dict)
-            return HttpResponse(result)
-
-def pollOrder(request):
-    return HttpResponse("pollOrder")
-
 def queryOrder(request):
-    return HttpResponse("queryOrder")
-
+    if not request.session.get("isSignIn", False):
+        return HttpResponseRedirect("sign")
+    if request.is_ajax() and request.method == "POST":
+        data = json.loads(request.body)
+        if "start_data" not in data \
+        or "stop_data" not in data:
+            return JsonResponse({"result":"fail"})
+        #orders = Order.objects.filter()
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
 def changeOrderStatus(request):
-    return HttpResponse("changeOrderStatus")
+    if not request.session.get("isSignIn", False):
+        return HttpResponseRedirect("sign")
+    if request.is_ajax() and request.method == "POST":
+        data = json.loads(request.body)
+        if "order_id" not in data \
+        or "type" not in data:
+            return JsonResponse({"result":"fail"})
+        try:
+            order = Order.objects.get(id=data[order])
+        except Exception:
+            return JsonResponse({"result":"fail"})
+        mType = data["type"]
+        global const
+        if mType == "accept":
+            order.status = const.order["ACCEPTED"]
+            return JsonResponse({"result":"success"})
+        elif mType == "sendout":
+            order.status = const.order["SENT"]
+            return JsonResponse({"result":"success"})
+        else:
+            return JsonResponse({"result":"fail"})
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+
+
 
 def logout(request):
-    return HttpResponse("logout")
+    if request.is_ajax() and request.method == "POST":
+        if "isSignIn" not in request.session:
+            return JsonResponse("result":"fail")
+        request.session["isSignIn"] = False
+        return JsonResponse("result":"success")
+    else:
+        return HttpResponseNotAllowed(['POST'], 'illegal request')
+
+        
 
 
