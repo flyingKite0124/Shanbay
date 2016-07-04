f16b0731ec556b33b71eefc7cc7d3120e154df87
Liqimai <liqimai@outlook.com>
2016-07-04 21:25:00 +0800

    Move ajax_fail_handler from manage.js to restaurant/base.js

--- static/restaurant/js/base.js
+++ static/restaurant/js/base.js
@@ -1,3 +1,12 @@
+/*请求失败时的回调函数，这个失败是指http的request失败，
+ *某些接口中给你返回{result:"fail"},并不会调用这个函数。
+ *你能成功收到这个{result:"fail"}返回，说明请求已经成功了*/
+function ajax_fail_handler(xhr, status, errorThrown) {
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+}
+
 function log_out(){
     $.ajax({
             url: "/restaurant/logout",   // 填上接口中要求的url
