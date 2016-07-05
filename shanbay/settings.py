d66ebb27ae07472e9b604eae0433025c6997e5ba
CodeBoat_zju <yizhou_cs_zju@qq.com>
2016-07-05 11:27:33 +0800

    yizhou setting

--- shanbay/settings.py
+++ shanbay/settings.py
@@ -82,10 +82,8 @@ WSGI_APPLICATION = 'shanbay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS':{
-            'read_default_file':os.path.join(os.path.dirname(__file__),'my.cnf'),
-        },
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
